0ef8bbffc5fe1f1cdf2d9da4656bf74f453fa1f4
MatheusGazzinelli <matheus.rosalobato@gmail.com>
2024-11-02 23:53:16 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,30 @@
+let dados = [
+    {
+        titulo: "Sobre mim",
+        descricao: "Sou Matheus Lobato, um estudante de 17 anos no segundo ano do ensino técnico no Coteig. Tenho uma paixão pela tecnologia e pela inovação, especialmente nas áreas de programação e design gráfico. Estou sempre em busca de novos desafios para aprimorar minhas habilidades e gosto muito de trabalhar em equipe, colaborando em projetos que buscam soluções criativas e eficientes. Meu objetivo é aplicar meu conhecimento em áreas que realmente façam a diferença no futuro.",
+        tags: "Sobre Mim, Carreira, Habilidades"
+    },
+    {
+        titulo: "Formação",
+        descricao: "Ensino técnico COTEMIG, aprendendo FrontEnd, C#, Banco de Dados e Ambientes Computacionais.",
+        
+        tags: "Formação, Cursos, Educação"
+    },
+    {
+        titulo: "Experiência",
+        descricao: "Me preparando para um estágio obrigatório de 4 - 6 meses.",
+        tags: "Experiência"
+    },
+    {
+        titulo: "Projetos",
+        descricao: "Próximo ano farei meu primeiro grande projeto, sendo ele meu trabalho de conclusão de curso. Enquanto isso, sigo fazendo meu melhor na escola..",
+        tags: "Projetos"
+    },
+    {
+        titulo: "Cursos",
+        descricao: "Além do ensino técnico, fazendo cursos da CISCO por meio de meu colégio.",
+        tags:"Cursos, Educação"
+
+    },
+
+];
